3408b2e6e2fc1455a2b4197b793ea0ec7c71ca92
Alex Nguyen <nguyeal2@oregonstate.edu>
2021-03-08 22:32:33 -0800

    added test2

--- fizz_test.py
+++ fizz_test.py
@@ -5,5 +5,8 @@ class test(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(fizz_code.fizz(5), ['1', '2', 'Fizz', '4', 'Buzz'])
 
+	def test2(self):
+		self.assertEqual(fizz_code.fizz(5), ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz'])
+
 if __name__ == '__main__':
 	unittest.main()
